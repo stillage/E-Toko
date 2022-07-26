b52dd28133531c232322f764368e604b3a71af24
stillage <hidayatarif690@gmail.com>
2022-07-26 15:51:36 +0700

    upload v3

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        <b>Version</b> 1.1
+        <b>Version</b> 1.0
     </div>
     <strong>Copyright &copy; {{ date('Y') }} <a href="/">{{ $setting->nama_perusahaan }}</a>.</strong> All rights
     reserved.
